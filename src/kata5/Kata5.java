8a7a544fd7a33e763b91fee8311047968823338e
irune <irune@MSI>
2019-01-20 17:46:52 +0000

    Kata5.1 version 2

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -2,6 +2,7 @@
 package kata5;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,43 +12,20 @@ import java.sql.Statement;
 public class Kata5 {
    
     public static void main(String[] args) {
-        connect();
-        selectAll();
-    }
-    private static void connect(){
-        Connection conn = null;
-        try {
-            String url = "jdbc:sqlite:Kata5.db";
-            conn = DriverManager.getConnection(url);
-            
-            
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
-    private static void selectAll(){
-        String sql = "SELECT * FROM PEOPLE";
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:Kata5.db");
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql)){
-            // Bucle sobre el conjunto de registros.
-            while (rs.next()) {
-                System.out.println(rs.getInt("id") + "\t" +
-                rs.getString("Nombre") + "\t" +
-                rs.getString("Apellido") + "\t" +
-                rs.getString("Departamento") + "\t");
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-}   
+        createNewDatabase("mail.db");
 
+    }
+    public static void createNewDatabase(String fileName) {
+    String url = "jdbc:sqlite:" + fileName;
+    try (Connection conn = DriverManager.getConnection(url)) {
+        if (conn != null) {
+            DatabaseMetaData meta = conn.getMetaData();
+            System.out.println("El driver es " + meta.getDriverName());
+            System.out.println("Se ha creado una nueva BD.");
+
+        }
+    } catch (SQLException e) {
+        System.out.println(e.getMessage());
+            }
+    }
+}
